74bcebb43306f5d7a1e8d80b7227e6fc90c11553
Yves / Stan <48611390+stanlee17@users.noreply.github.com>
2023-08-21 23:59:33 +1000

    offcanvas/sidebar added on mobile nav

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -1,12 +1,28 @@
+import { useState, useEffect } from 'react';
 import styles from './Navbar.module.scss';
 import { Link } from 'react-router-dom';
 import LinkButton from '../../common/LinkButton/LinkButton';
 import Logo from '../../../assets/logo/logo.svg';
 import Hamburger from '../../../assets/icons/icon-hamburger.svg';
+import Close from '../../../assets/icons/icon-close.svg';
 
 const Navbar = () => {
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
+
+  useEffect(() => {
+    if (sidebar) {
+      document.documentElement.style.overflowY = 'hidden';
+      document.body.style.overflowY = 'hidden';
+    } else {
+      document.documentElement.style.overflowY = 'auto';
+      document.body.style.overflowY = 'auto';
+    }
+  }, [sidebar]);
+
   return (
-    <div className={styles.navbar}>
+    <nav className={styles.navbar}>
       <div className={styles.navLinks}>
         {/* LOGO */}
         <Link to="/" className={styles.logo}>
@@ -15,8 +31,12 @@ const Navbar = () => {
 
         {/* LINKS: Home & About */}
         <div className={styles.links}>
-          <Link to="/">home</Link>
-          <Link to="/about">about</Link>
+          <Link to="/" className={styles.link}>
+            home
+          </Link>
+          <Link to="/about" className={styles.link}>
+            about
+          </Link>
         </div>
       </div>
 
@@ -24,10 +44,46 @@ const Navbar = () => {
         contact us
       </LinkButton>
 
-      <div className={styles.hamburger}>
+      <div className={styles.hamburger} onClick={showSidebar}>
         <img src={Hamburger} alt="hamburger-icon" />
       </div>
-    </div>
+
+      {/* Offcanvas/Sidebar */}
+      <div
+        className={
+          sidebar ? `${styles.overlay} ${styles.active}` : `${styles.overlay}`
+        }
+        onClick={showSidebar}
+      ></div>
+      <nav
+        className={
+          sidebar ? `${styles.sidebar} ${styles.active}` : `${styles.sidebar}`
+        }
+      >
+        <div className={styles.menuItems}>
+          <div className={styles.sidebarToggle} onClick={showSidebar}>
+            <img
+              src={Close}
+              alt="icon-menu-close"
+              className={styles.menuClose}
+            />
+          </div>
+
+          <div className={styles.menuLinks}>
+            <Link to="/" className={styles.menuLink}>
+              home
+            </Link>
+            <Link to="/about" className={styles.menuLink}>
+              about
+            </Link>
+          </div>
+
+          <LinkButton to="/contact" className={styles.menuButton}>
+            contact us
+          </LinkButton>
+        </div>
+      </nav>
+    </nav>
   );
 };
 
