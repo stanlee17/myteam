733589a9c24766e6328dd08c9607c7799acbc669
Yves / Stan <48611390+stanlee17@users.noreply.github.com>
2023-08-25 10:51:46 +1000

    favicon added

--- src/components/common/Container/Container.tsx
+++ src/components/common/Container/Container.tsx
@@ -3,10 +3,11 @@ import styles from './Container.module.scss';
 
 interface ContainerProps {
   children: ReactNode;
+  className?: string;
 }
 
-const Container = ({ children }: ContainerProps) => {
-  return <div className={styles.container}>{children}</div>;
+const Container = ({ children, className }: ContainerProps) => {
+  return <div className={`${styles.container} ${className}`}>{children}</div>;
 };
 
 export default Container;
